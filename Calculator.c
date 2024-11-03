363bd0d42ac7c23be08aad962c3f6ae2a4e1168a
Ali Mwabashiri <143745832+Abually@users.noreply.github.com>
2024-11-03 09:22:53 +0300

    Create Calculator.c

---
+++ Calculator.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+ int main()
+
+ {
+
+ /*variable declaration*/
+
+ char operation;
+
+ int num1,num2;
+
+ double results;
+
+ 
+
+ /*code for seeking user input*/
+
+ printf("Enter operator:");
+
+ scanf("%c",&operation);
+
+ printf("Enter value 1:");
+
+ scanf("%d",&num1);
+
+ printf("Enter value 2:");
+
+ scanf("%d",&num2);
+
+ 
+
+ 
+
+ 
+
+ /*conditions*/
+
+ if(operation=='+')
+
+ {results=num1+num2;
+
+ }
+
+ else if(operation=='-')
+
+ { 
+
+ results=num1-num2;
+
+}
+
+else if(operation=='*')
+
+{
+
+results=num1*num2;}
+
+else if(operation=='/')
+
+{
+
+results=num1/num2;
+
+}
+
+/*output */
+
+printf("The Answer:%f",results);
+
+return 0;
+}
